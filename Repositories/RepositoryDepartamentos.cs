6cae65a24b91b2347e3c32d796ba4114473d21a4
alejandropj <alejandroparrajimenez@gmail.com>
2024-05-10 11:48:13 +0200

    Add project files.

---
+++ Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreAWSPostgresEC2.Data;
+using MvcCoreAWSPostgresEC2.Models;
+
+namespace MvcCoreAWSPostgresEC2.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamentos(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Departamento>>
+            GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<Departamento>
+            FindDepartamentoAsync(int id)
+        {
+            return await this.context.Departamentos
+                .FirstOrDefaultAsync(x => x.IdDepartamento == id);
+        }
+    }
+}
